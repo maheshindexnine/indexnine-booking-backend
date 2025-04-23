8054694c3503832d51a42ca7d17e249d5190e36c
Sushrita Banerjee <mahesh.gaikwad@indexnine.com>
2025-04-23 12:09:01 +0530

    Connected to mongodb atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/indexnine-booking'),
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes env config available everywhere
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+    }),
     UserModule,
   ],
   controllers: [AppController],
