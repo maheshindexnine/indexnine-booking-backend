7578869f06c994e72bf108de7f96cb31fddff214
Sushrita Banerjee <mahesh.gaikwad@indexnine.com>
2025-05-12 15:32:14 +0530

    Added FE url in cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:3001'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  });
+
   if (process.env.ENABLE_KAFKA === 'true') {
     app.connectMicroservice<MicroserviceOptions>({
       transport: Transport.KAFKA,
